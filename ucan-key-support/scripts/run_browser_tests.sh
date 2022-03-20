08d9ca8364b61e7dcd132f6b0204bf56bde7f173
Chris Joel <0xcda7a@gmail.com>
2022-03-19 22:15:38 -0700

    Maybe just trick the test runner?

--- ucan-key-support/scripts/run_browser_tests.sh
+++ ucan-key-support/scripts/run_browser_tests.sh
@@ -43,7 +43,7 @@ else
     echo $WEBDRIVER | jq ". + $BROWSERSTACK_SESSION" > webdriver.json
 
     cargo build --target wasm32-unknown-unknown --features web
-    RUST_LOG=debug CHROMEDRIVER_REMOTE=http://hub-cloud.browserstack.com/wd/hub/ \
+    RUST_LOG=debug GECKODRIVER_REMOTE=http://hub-cloud.browserstack.com/wd/hub \
         cargo test --target wasm32-unknown-unknown --features web -- --nocapture
 
     set +x
