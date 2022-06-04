5d80833c545a57f3cdd23781cf890854d69a0439
Christopher Joel <240083+cdata@users.noreply.github.com>
2022-06-04 10:10:36 -0700

    Make Ucan serializable (#8)

--- ucan/src/tests/ucan.rs
+++ ucan/src/tests/ucan.rs
@@ -1,6 +1,8 @@
 mod validate {
+    use did_key::KeyPair;
+
     use crate::{
-        builder::UcanBuilder,
+        builder::{Signable, UcanBuilder},
         crypto::did::DidParser,
         tests::fixtures::{Identities, SUPPORTED_KEYS},
         time::now,
@@ -61,5 +63,44 @@ mod validate {
             .unwrap();
 
         assert!(!ucan.is_too_early());
+    }
+
+    #[tokio::test]
+    async fn it_can_be_serialized_as_json() {
+        let identities = Identities::new().await;
+        let ucan = UcanBuilder::default()
+            .issued_by(&identities.alice_key)
+            .for_audience(identities.bob_did.as_str())
+            .not_before(now() / 1000)
+            .with_lifetime(30)
+            .build()
+            .unwrap()
+            .sign()
+            .await
+            .unwrap();
+
+        let ucan_json = serde_json::to_value(ucan.clone()).unwrap();
+
+        assert_eq!(
+            ucan_json,
+            serde_json::json!({
+                "header": {
+                    "alg": "EdDSA",
+                    "typ": "JWT",
+                    "ucv": Signable::<KeyPair>::UCAN_VERSION
+                },
+                "payload": {
+                    "iss": ucan.issuer(),
+                    "aud": ucan.audience(),
+                    "exp": ucan.expires_at(),
+                    "nbf": ucan.not_before(),
+                    "att": [],
+                    "fct": [],
+                    "prf": []
+                },
+                "signed_data": ucan.signed_data(),
+                "signature": ucan.signature()
+            })
+        );
     }
 }
