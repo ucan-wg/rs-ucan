66fc3ae195997043719ad9663ff5300f719eb850
Chris Joel <0xcda7a@gmail.com>
2022-10-02 17:54:34 -0700

    Add default implementations for KeyMaterial

--- ucan/src/crypto/key.rs
+++ ucan/src/crypto/key.rs
@@ -1,3 +1,5 @@
+use std::sync::Arc;
+
 use anyhow::Result;
 use async_trait::async_trait;
 
@@ -32,4 +34,47 @@ pub trait KeyMaterial: KeyMaterialConditionalSendSync {
 
     /// Verify the alleged signature of some data against this key
     async fn verify(&self, payload: &[u8], signature: &[u8]) -> Result<()>;
+}
+
+#[cfg_attr(not(target_arch = "wasm32"), async_trait)]
+#[cfg_attr(target_arch = "wasm32", async_trait(?Send))]
+impl KeyMaterial for Box<dyn KeyMaterial> {
+    fn get_jwt_algorithm_name(&self) -> String {
+        self.as_ref().get_jwt_algorithm_name()
+    }
+
+    async fn get_did(&self) -> Result<String> {
+        self.as_ref().get_did().await
+    }
+
+    async fn sign(&self, payload: &[u8]) -> Result<Vec<u8>> {
+        self.as_ref().sign(payload).await
+    }
+
+    async fn verify(&self, payload: &[u8], signature: &[u8]) -> Result<()> {
+        self.as_ref().verify(payload, signature).await
+    }
+}
+
+#[cfg_attr(not(target_arch = "wasm32"), async_trait)]
+#[cfg_attr(target_arch = "wasm32", async_trait(?Send))]
+impl<K> KeyMaterial for Arc<K>
+where
+    K: KeyMaterial,
+{
+    fn get_jwt_algorithm_name(&self) -> String {
+        (**self).get_jwt_algorithm_name()
+    }
+
+    async fn get_did(&self) -> Result<String> {
+        (**self).get_did().await
+    }
+
+    async fn sign(&self, payload: &[u8]) -> Result<Vec<u8>> {
+        (**self).sign(payload).await
+    }
+
+    async fn verify(&self, payload: &[u8], signature: &[u8]) -> Result<()> {
+        (**self).verify(payload, signature).await
+    }
 }
