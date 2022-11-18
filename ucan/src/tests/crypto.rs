36516beafe1efb2514d8080eb4ede37f2d71cd23
Zeeshan Lakhani <zeeshan.lakhani@gmail.com>
2022-11-18 10:44:09 -0500

    chore: deps update to minor versions (float patch) and did-key workaround

--- ucan/src/tests/crypto.rs
+++ ucan/src/tests/crypto.rs
@@ -1,5 +1,5 @@
 mod did_from_keypair {
-    use did_key::{Bls12381KeyPairs, Ed25519KeyPair, Generate, KeyPair};
+    use did_key::{from_existing_key, Ed25519KeyPair, KeyMaterial as _KeyMaterial};
 
     use crate::crypto::KeyMaterial;
 
@@ -13,23 +13,10 @@ mod did_from_keypair {
     #[cfg_attr(not(target_arch = "wasm32"), tokio::test)]
     async fn it_handles_ed25519_keys() {
         let pub_key = base64::decode("Hv+AVRD2WUjUFOsSNbsmrp9fokuwrUnjBcr92f0kxw4=").unwrap();
-        let keypair = KeyPair::Ed25519(Ed25519KeyPair::from_public_key(&pub_key));
+        let key = Ed25519KeyPair::from_public_key(&pub_key);
+        let keypair = from_existing_key::<Ed25519KeyPair>(&key.public_key_bytes(), None);
 
         let expected_did = "did:key:z6MkgYGF3thn8k1Fv4p4dWXKtsXCnLH7q9yw4QgNPULDmDKB";
-        let result_did = keypair.get_did().await.unwrap();
-
-        assert_eq!(expected_did, result_did.as_str());
-    }
-
-    #[allow(dead_code)]
-    // #[cfg_attr(target_arch = "wasm32", wasm_bindgen_test)]
-    #[ignore = "Public key is allegedly invalid size"]
-    #[cfg_attr(not(target_arch = "wasm32"), tokio::test)]
-    async fn it_handles_bls12381_keys() {
-        let pub_key = base64::decode("Hv+AVRD2WUjUFOsSNbsmrp9fokuwrUnjBcr92f0kxw4=").unwrap();
-        let keypair = KeyPair::Bls12381G1G2(Bls12381KeyPairs::from_public_key(&pub_key));
-
-        let expected_did = "did:key:z6HpYD1br5P4QVh5rjRGAkBfKMWes44uhKmKdJ6dN2Nm9gHK";
         let result_did = keypair.get_did().await.unwrap();
 
         assert_eq!(expected_did, result_did.as_str());
--- ucan/src/tests/fixtures/identities.rs
+++ ucan/src/tests/fixtures/identities.rs
@@ -1,10 +1,13 @@
+use did_key::{
+    from_existing_key, Ed25519KeyPair, Generate, KeyMaterial as _KeyMaterial, PatchedKeyPair,
+};
+
 use crate::crypto::KeyMaterial;
-use did_key::{Ed25519KeyPair, Generate, KeyPair};
 
 pub struct Identities {
-    pub alice_key: KeyPair,
-    pub bob_key: KeyPair,
-    pub mallory_key: KeyPair,
+    pub alice_key: PatchedKeyPair,
+    pub bob_key: PatchedKeyPair,
+    pub mallory_key: PatchedKeyPair,
 
     pub alice_did: String,
     pub bob_did: String,
@@ -17,18 +20,30 @@ impl Identities {
     pub async fn new() -> Self {
         // NOTE: tweetnacl secret keys concat the public keys, so we only care
         // about the first 32 bytes
-        let alice_key = KeyPair::from(Ed25519KeyPair::from_secret_key(&base64::decode("U+bzp2GaFQHso587iSFWPSeCzbSfn/CbNHEz7ilKRZ1UQMmMS7qq4UhTzKn3X9Nj/4xgrwa+UqhMOeo4Ki8JUw==".as_bytes()).unwrap().as_slice()[0..32]));
-        let bob_key = KeyPair::from(Ed25519KeyPair::from_secret_key(&base64::decode("G4+QCX1b3a45IzQsQd4gFMMe0UB1UOx9bCsh8uOiKLER69eAvVXvc8P2yc4Iig42Bv7JD2zJxhyFALyTKBHipg==".as_bytes()).unwrap().as_slice()[0..32]));
-        let mallory_key = KeyPair::from(Ed25519KeyPair::from_secret_key(&base64::decode("LR9AL2MYkMARuvmV3MJV8sKvbSOdBtpggFCW8K62oZDR6UViSXdSV/dDcD8S9xVjS61vh62JITx7qmLgfQUSZQ==".as_bytes()).unwrap().as_slice()[0..32]));
+        let alice_key = Ed25519KeyPair::from_secret_key(&base64::decode("U+bzp2GaFQHso587iSFWPSeCzbSfn/CbNHEz7ilKRZ1UQMmMS7qq4UhTzKn3X9Nj/4xgrwa+UqhMOeo4Ki8JUw==".as_bytes()).unwrap().as_slice()[0..32]);
+        let alice_keypair = from_existing_key::<Ed25519KeyPair>(
+            &alice_key.public_key_bytes(),
+            Some(&alice_key.private_key_bytes()),
+        );
+        let bob_key = Ed25519KeyPair::from_secret_key(&base64::decode("G4+QCX1b3a45IzQsQd4gFMMe0UB1UOx9bCsh8uOiKLER69eAvVXvc8P2yc4Iig42Bv7JD2zJxhyFALyTKBHipg==".as_bytes()).unwrap().as_slice()[0..32]);
+        let bob_keypair = from_existing_key::<Ed25519KeyPair>(
+            &bob_key.public_key_bytes(),
+            Some(&bob_key.private_key_bytes()),
+        );
+        let mallory_key = Ed25519KeyPair::from_secret_key(&base64::decode("LR9AL2MYkMARuvmV3MJV8sKvbSOdBtpggFCW8K62oZDR6UViSXdSV/dDcD8S9xVjS61vh62JITx7qmLgfQUSZQ==".as_bytes()).unwrap().as_slice()[0..32]);
+        let mallory_keypair = from_existing_key::<Ed25519KeyPair>(
+            &mallory_key.public_key_bytes(),
+            Some(&mallory_key.private_key_bytes()),
+        );
 
         Identities {
-            alice_did: alice_key.get_did().await.unwrap(),
-            bob_did: bob_key.get_did().await.unwrap(),
-            mallory_did: mallory_key.get_did().await.unwrap(),
+            alice_did: alice_keypair.get_did().await.unwrap(),
+            bob_did: bob_keypair.get_did().await.unwrap(),
+            mallory_did: mallory_keypair.get_did().await.unwrap(),
 
-            alice_key,
-            bob_key,
-            mallory_key,
+            alice_key: alice_keypair,
+            bob_key: bob_keypair,
+            mallory_key: mallory_keypair,
         }
     }
 
