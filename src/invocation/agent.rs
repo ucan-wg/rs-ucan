643b4662b8f5879963eafe2c9f3ce6410f980535
Brooklyn Zelenka <hello@brooklynzelenka.com>
2024-03-21 16:05:04 -0700

    Correct the `FIXME` for command

--- src/invocation/agent.rs
+++ src/invocation/agent.rs
@@ -88,7 +88,6 @@ where
         &self,
         audience: Option<DID>,
         subject: DID,
-        // command: String,
         ability: T,
         metadata: BTreeMap<String, Ipld>,
         cause: Option<Cid>,
@@ -99,7 +98,13 @@ where
     ) -> Result<Invocation<T, DID, V, C>, InvokeError<D::DelegationStoreError>> {
         let proofs = self
             .delegation_store
-            .get_chain(&self.did, &Some(subject.clone()), "/".into(), vec![], now) // FIXME
+            .get_chain(
+                &self.did,
+                &Some(subject.clone()),
+                ability.to_command(),
+                vec![],
+                now,
+            )
             .map_err(InvokeError::DelegationStoreError)?
             .map(|chain| chain.map(|(cid, _)| cid).into())
             .unwrap_or(vec![]);
