bc0a275fab07ec246f75a08e067e75b8a6268a4f
Brooklyn Zelenka <hello@brooklynzelenka.com>
2024-03-25 19:21:42 -0700

    Add automatic CID conversaion error

--- src/delegation/store/traits.rs
+++ src/delegation/store/traits.rs
@@ -11,6 +11,7 @@ use libipld_core::ipld::Ipld;
 use libipld_core::{cid::Cid, codec::Codec};
 use nonempty::NonEmpty;
 use std::{fmt::Debug, sync::Arc};
+use thiserror::Error;
 use web_time::SystemTime;
 
 pub trait Store<DID: Did + Clone, V: varsig::Header<C> + Clone, C: Codec + TryFrom<u64> + Into<u64>>
@@ -26,8 +27,12 @@ where
         cid: &Cid,
     ) -> Result<Option<Arc<Delegation<DID, V, C>>>, Self::DelegationStoreError>;
 
-    fn insert(&self, delegation: Delegation<DID, V, C>) -> Result<(), Self::DelegationStoreError> {
-        self.insert_keyed(delegation.cid().expect("FIXME"), delegation)
+    fn insert(
+        &self,
+        delegation: Delegation<DID, V, C>,
+    ) -> Result<(), CannotCidOr<Self::DelegationStoreError>> {
+        self.insert_keyed(delegation.cid()?, delegation)
+            .map_err(CannotCidOr::StoreError)
     }
 
     fn insert_keyed(
@@ -127,4 +132,13 @@ where
     {
         (**self).get_chain(audience, subject, command, policy, now)
     }
+}
+
+#[derive(Debug, Error)]
+pub enum CannotCidOr<E> {
+    #[error("Cannot make CID from delegation based on supplied Varsig")]
+    CannotMakeCid(#[from] libipld_core::error::Error),
+
+    #[error("Store error: {0}")]
+    StoreError(E),
 }
